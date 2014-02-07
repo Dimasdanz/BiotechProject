bb0ef5882855d4c1dce356caf5cfa2bbc3c3330d
Dimas R. Danu <dimasdanz@gmail.com>
2014-02-07 15:11:38 +0700

    URL Fix

--- BiotechWeb/application/views/gcs/gcs_home.php
+++ BiotechWeb/application/views/gcs/gcs_home.php
@@ -2,7 +2,28 @@
 <?php
 	$this->load->view('gcs/gcs_header');
 ?>
-
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<i class="fa fa-bar-chart-o"></i> Temperature
+				</div>
+				<div class="panel-body">
+					
+				</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="panel panel-primary">
+				<div class="panel-heading">
+					<i class="fa fa-bar-chart-o"></i> Lux
+				</div>
+				<div class="panel-body">
+					
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 <?php
 	$this->load->view('template/footer');
