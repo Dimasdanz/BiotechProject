ac84a6f54dcd3754d6316a3d3fcfc80e9c9ae3ea
rickyfm <rickyfm1993@gmail.com>
2014-02-18 11:25:59 +0700

    scs_update

--- BiotechHardware/scs/scs.ino
+++ BiotechHardware/scs/scs.ino
@@ -6,7 +6,8 @@
 #include <EthernetClient.h>
 
 const int smoke_pin = A0;
-const int temp_pin =  2;
+const int temp_pin =  6;
+const int Relay = 2; 
 
 byte mac[] = {0x90, 0xA2, 0xDA, 0x0E, 0xF5, 0x30 };
 const char server[] = "192.168.1.3";
@@ -21,14 +22,29 @@ void setup() {
   temp_sensor.begin();
   Ethernet.begin(mac, ip);
   delay(1000);
+  pinMode(Relay, OUTPUT);      
 }
 
 void loop() {
   temp_sensor.requestTemperatures();
-  Serial.println(analogRead(smoke_pin));  
-  Serial.println(temp_sensor.getTempCByIndex(0));
-  send_log(temp_sensor.getTempCByIndex(0), analogRead(smoke_pin));
-  delay(5000);
+  
+  float temp = temp_sensor.getTempCByIndex(0);
+  float smoke = analogRead(smoke_pin);
+
+  
+  if(temp > 30 || smoke > 400){
+    digitalWrite(Relay, LOW);
+  }
+  else{
+    digitalWrite(Relay, HIGH);
+  }
+
+  Serial.println(smoke);  
+  Serial.println(temp);
+  send_log(temp, smoke);
+  
+  
+  delay(1000);
 }
 
 void send_log(float temp, float smoke){
