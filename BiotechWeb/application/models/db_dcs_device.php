029c54a2fa7f52fd9ea7656a1448d9c60171c9bb
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-19 16:13:43 +0700

    Modify device model

--- BiotechWeb/application/models/db_dcs_device.php
+++ BiotechWeb/application/models/db_dcs_device.php
@@ -5,7 +5,22 @@ class db_dcs_device extends CI_Model{
 		return $q->result();
 	}
 	
+	function get_single($id){
+		$this->db->where('user_id', $id);
+		$q = $this->db->get('dcs_device');
+		if($q->num_rows() > 0){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
 	function insert($data){
 		$this->db->insert('dcs_device', $data);
+	}
+	
+	function delete($id){
+		$this->db->where('user_id', $id);
+		$this->db->delete('dcs_device');
 	}
 }
