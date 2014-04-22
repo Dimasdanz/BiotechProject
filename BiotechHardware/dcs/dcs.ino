cc772222977921bcf365d4118435289b035f3119
Dimas R. Danu <dimasdanz@gmail.com>
2014-04-22 21:37:23 +0700

    Everything is finish, LCD print left

--- BiotechHardware/dcs/dcs.ino
+++ BiotechHardware/dcs/dcs.ino
@@ -13,21 +13,22 @@ byte ip[] = {
 
 EthernetServer server(80);
 boolean device_status = true;
+boolean isInputting = false;
 
 const byte ROWS = 4;
 const byte COLS = 3;
 char keys[ROWS][COLS] = {
   {
-    '1','2','3'  }
+    '1','2','3'    }
   ,
   {
-    '4','5','6'  }
+    '4','5','6'    }
   ,
   {
-    '7','8','9'  }
+    '7','8','9'    }
   ,
   {
-    '*','0','#'  }
+    '*','0','#'    }
 };
 
 byte rowPins[ROWS] = {
@@ -39,17 +40,22 @@ Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 LiquidCrystal_I2C lcd(0x27,20,4);
 Servo myservo;
 
-char pass[16];
+char pass[10];
 int attempt = 0;
 int count = 0;
-int max_attempt = 3;
 
 const int sensor = A2;
 const int push_btn = A3;
 const int servo = 9;
 
+long prevTime = 0;
+long idleTime = 0;
+long interval_reset = 180000;
+long interval_idle = 20000;
+
 void setup(){
   Serial.begin(9600);
+  myservo.attach(9);
   Ethernet.begin(mac, ip);
   server.begin();
   pinMode(sensor, INPUT);
@@ -57,7 +63,7 @@ void setup(){
   pinMode(push_btn, INPUT);
   digitalWrite(push_btn, HIGH);
   myservo.write(70);
-  Serial.println("ready");
+  Serial.println("Ready");
 }
 
 void loop(){
@@ -74,6 +80,7 @@ void loop(){
               client.println("Content-Type: application/json");
               client.println();
               client.print("{\"response\":\"open\"}");
+              open_door();
             }
             else if(type == 's'){
               int val = client.parseInt();
@@ -121,6 +128,13 @@ void loop(){
                 client.print("{\"response\":\"non-active\"}");
               }
             }
+            else if(type == 'f') {
+              client.println("HTTP/1.1 200 OK");
+              client.println("Content-Type: application/json");
+              client.println();
+              client.print("{\"response\":\"false\"}");
+              wrong_password();
+            }
             else {
               Serial.print("Unexpected type ");
               Serial.print(type);
@@ -137,18 +151,21 @@ void loop(){
     delay(10);
     client.stop();
   }
+
   if(digitalRead(push_btn) == LOW){
     auth_user("keluar");
     open_door();
   }
-
-  if(device_status){
+  
+  if(device_status && !device_isLock()){
     char key = keypad.getKey();
-    if (key != NO_KEY && key != '*'){
+    if (key != NO_KEY && key != '*' && key != '#'){
+      isInputting = true;
       pass[count] = key;
       count++;
     }
     if(key == '*'){
+      isInputting = false;
       attempt++;
       auth_user(pass);
       sys_init();
@@ -156,22 +173,45 @@ void loop(){
     if(key == '#'){
       sys_init();
     }
+    if(count > 9){
+      auth_user(pass);
+      sys_init();
+    }
+  }
+  else{
+    //Device Non-Active
+  }
+
+  unsigned long curTime = millis();
+  if(curTime - idleTime > interval_idle){
+    idleTime = curTime;
+    isInputting = false;
+    sys_init();
+  }
+
+  if(!isInputting){
+    if(curTime - prevTime > interval_reset){
+      prevTime = curTime;
+      attempt = 0;
+    }
   }
 }
 
 void sys_init(){
-  memset(pass, 0, sizeof pass);
+  Serial.println("sys_init()");
+  Serial.println(pass);
+  memset(pass, 0, 10);
   count = 0;
+  Serial.println(pass);
 }
 
 void auth_user(char user_input[]){
   EthernetClient client;
-  Serial.println("Sending");
+  Serial.println(user_input);
   if (client.connect(server_addr,80))
   {
     client.print("GET /api/dcs/dcs_auth/");
     client.print(user_input);
-    Serial.println(user_input);
     client.print(" HTTP/1.1\n");
     client.print("Host: 192.168.2.4\n");
     client.print("Connection: close\n\n");
@@ -179,11 +219,42 @@ void auth_user(char user_input[]){
   }
   else{
     Serial.println("Sending failed");
+    String pass(user_input);
+    if(pass == "123"){
+      if(pass != "keluar"){
+        open_door();
+      }
+    }
+    else{
+      wrong_password();
+    }
   }
 }
 
 void open_door(){
   Serial.println("Door open");
+  attempt = 0;
+  myservo.write(30);
+  delay(3000);
+  while(digitalRead(sensor) == HIGH);
   delay(1000);
+  myservo.write(70);
+  Serial.println("Door closed");
+  idleTime = millis();
 }
 
+boolean device_isLock(){
+  if(attempt >= 3){
+    //Serial.println("Device lock");
+    return true;
+  }
+  else{
+    //Serial.println("Device unlock");
+    return false;
+  }
+}
+
+void wrong_password(){
+  Serial.println("Wrong Password");
+  idleTime = millis();
+}
